3e90eb6bbe26ef321bf64bb6091b997cd2ec2c19
Al3xDo <56794124+Al3xDo@users.noreply.github.com>
2022-05-15 16:38:37 +0700

    add chart adn sidebar chart

--- web_order_food/dashboard/views.py
+++ web_order_food/dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from menu.models import food, cart, category
 from collections import defaultdict
+from django.db.models.functions import ExtractYear
 def get_12_month_prev():
     pass
 def get_season_prev():
@@ -17,24 +18,53 @@ class dashboard(View):
         for cart_item in carts:
             id_foods= cart_item.id_foods.split(",")
             for id_food in id_foods:
-                price=int(food.objects.get(id = str(id_food), active=1).price)
+                query=food.objects.get(id = str(id_food), active=1)
+                price=int(query.price)
                 total_revenue +=price
                 total_food_sold+=1
-                categories_sold[id_food]+= price
+                categories_sold[query.category_food.category]+= 1 
         # get the total category
         total_category= category.objects.count()
         # get the total food sold item
         # get the total customer
         total_customer= cart.objects.values('user_name').distinct().count()
-        # filter out top 5 food sold weekly(up to that day)
 
+        # filter out top 5 food sold weekly(up to that day)
+        # total category sold for each period
+        category_list=[]
+        for query in category.objects.values("category"):
+            category_list.append(query['category'])
+        total_carts = cart.objects.filter(paid_bill=True).annotate(year= ExtractYear("date_created")).order_by("year").values("year","id_foods")
+        extra_area_chart_var_dict={}
+        total_category_sold=0
+        for cart_item in total_carts:
+            if cart_item['year'] not in extra_area_chart_var_dict.keys():
+                extra_area_chart_var_dict[cart_item['year']]=defaultdict(int)
+            for id_food in cart_item["id_foods"].split(","):
+                    query= food.objects.get(id=id_food)    
+                    extra_area_chart_var_dict[cart_item['year']][query.category_food.category]+=1
+                    total_category_sold+=1
+        extra_area_chart_var_list=[]
+        for i in extra_area_chart_var_dict.keys():
+            var_dict= {
+                "period": str(i)
+            }
+            for k,v in extra_area_chart_var_dict[i].items(): 
+                var_dict[k]=v
+            for category_item in category_list:
+                if category_item not in var_dict.keys():
+                    var_dict[category_item]=0
+            extra_area_chart_var_list.append(var_dict)
+        # calculate the percentage of category sold
+        for category_ in category_list:
+            categories_sold[category_]= round((categories_sold[category_] / total_category_sold) * 100)
         # get pending user cart
         pending_carts= cart.objects.filter(statement_bill=0).all()
         pending_carts_list=[]
         for cart_item in pending_carts:
             total_price=0
             name_food_boughts=[]
-            for id_food in cart_item.id_foods:
+            for id_food in cart_item.id_foods.split(","):
                 food_query= food.objects.get(id=id_food)
                 name_food_boughts.append(food_query.name_food)
                 total_price+=int(food_query.price)
@@ -45,14 +75,16 @@ class dashboard(View):
                 "Status": cart.statement_bill_choice[0][1]
             }
             pending_carts_list.append(pending_cart_item)
-            
         return render(request, 'index.html',
                 {
                     "total_revenue": total_revenue,
                     "total_food_sold": total_food_sold,
                     "total_category": total_category,
                     "total_customer": total_customer,
-                    "pending_cart_list": pending_carts_list
+                    "pending_cart_list": pending_carts_list,
+                    "extra_area_chart_var_list": extra_area_chart_var_list,
+                    "category_list": category_list,
+                    "categories_sold": categories_sold,
                 })
 class analytic(View):
     def get(self,request):
--- web_order_food/static/dashboard/js/lib/morris-chart/dashboard1-init.js
+++ web_order_food/static/dashboard/js/lib/morris-chart/dashboard1-init.js
@@ -1,68 +1,73 @@
 // Dashboard 1 Morris-chart
-$( function () {
+$(function () {
 	"use strict";
 
 
 	// Extra chart
-	Morris.Area( {
+	Morris.Area({
 		element: 'extra-area-chart',
-		data: [ {
+		data: [
+			// {% for item in count %}
+			// 	{ y: '{{ item.date|date:"Y" }}', a: '{{ item.count }}' }
+			// {% if not forloop.last %},{% endif %}
+			// {% endfor %}
+			{
 				period: '2001',
 				iphone: 0,
 				imac: 0,
 				ibook: 90,
 				samsung: 0,
 				android: 0
-        }, {
+			}, {
 				period: '2002',
 				iphone: 10,
 				imac: 60,
 				ibook: 40,
 				samsung: 80,
 				android: 120
-        }, {
+			}, {
 				period: '2003',
 				iphone: 120,
 				imac: 10,
 				ibook: 90,
 				samsung: 30,
 				android: 50
-        }, {
+			}, {
 				period: '2004',
 				iphone: 0,
 				imac: 0,
 				ibook: 120,
 				samsung: 0,
 				android: 0
-        }, {
+			}, {
 				period: '2005',
 				iphone: 0,
 				imac: 0,
 				ibook: 0,
 				samsung: 150,
 				android: 0
-        }, {
+			}, {
 				period: '2006',
 				iphone: 160,
 				imac: 75,
 				ibook: 30,
 				samsung: 60,
 				android: 90
-        }, {
+			}, {
 				period: '2007',
 				iphone: 10,
 				imac: 120,
 				ibook: 40,
 				samsung: 60,
 				android: 30
-        }
+			}
 
 
-        ],
-		lineColors: [ '#26DAD2', '#fc6180', '#62d1f3', '#ffb64d', '#4680ff' ],
+		],
+		lineColors: ['#26DAD2', '#fc6180', '#62d1f3', '#ffb64d', '#4680ff'],
 		xkey: 'period',
-		ykeys: [ 'iphone', 'imac', 'ibook', 'samsung', 'android' ],
-		labels: [ 'iphone', 'imac', 'ibook', 'samsung', 'android' ],
+		ykeys: ['iphone', 'imac', 'ibook', 'samsung', 'android'],
+		labels: ['iphone', 'imac', 'ibook', 'samsung', 'android'],
 		pointSize: 0,
 		lineWidth: 0,
 		resize: true,
@@ -71,8 +76,8 @@ $( function () {
 		gridLineColor: '#e0e0e0',
 		hideHover: 'auto'
 
-	} );
+	});
 
 
 
-} );
+});

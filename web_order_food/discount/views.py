be4e07698e428a70887ff1f534b6c1dca7656d32
ThanhNguyenVan06 <84319151+ThanhNguyenVan06@users.noreply.github.com>
2022-05-16 11:02:43 +0700

    update

--- web_order_food/discount/views.py
+++ web_order_food/discount/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import discounts
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+
+def render_discount(request):
+    coupons = discounts.objects.all()
+    coupons_paginator = Paginator(coupons,2)
+    index_page = request.GET.get('page')
+    page = coupons_paginator.get_page(index_page)
+    context = { 'count' : coupons_paginator.count,
+               'page':page,
+        
+    }
+    return render(request, 'discount.html', context)
